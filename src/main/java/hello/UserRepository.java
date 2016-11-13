1109a23d27866f70503aaef5add09b16aa0c9047
Kjell De Backer <kjell.debacker@student.odisee.be>
2016-11-13 21:07:12 +0100

    Fixed a couple of typos

--- src/main/java/hello/UserRepository.java
+++ src/main/java/hello/UserRepository.java
@@ -1,7 +1,7 @@
 package hello;
 
-import org.springframework.data.mongodb.repostitory.MongoRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepostitory extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String> {
     User findByName(String name);
 }
