b32df4b079780f87b28bbd2c9730fb51a95631ac
Kjell De Backer <kjell.debacker@student.odisee.be>
2016-11-15 19:37:14 +0100

    Removed the hello package

--- src/main/java/hello/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByName(String name);
-}
