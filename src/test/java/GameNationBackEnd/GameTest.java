a64294b76c4f85a556a58b4a7d2bc89d38966c23
lucas <lucas.coupez@gmail.com>
2016-11-17 18:14:46 +0100

    Remove intentionally failing test

--- src/test/java/GameNationBackEnd/GameTest.java
+++ src/test/java/GameNationBackEnd/GameTest.java
@@ -10,21 +10,11 @@ import static org.junit.Assert.*;
 public class GameTest {
 
     @Test
-    public void FirstPassingTest() {
+    public void NewGameCustomConstructor() {
         Game game = new Game("hey", "some description", "wuk.jpg");
 
         assertEquals("hey", game.getName());
         assertEquals("some description", game.getDescription());
         assertEquals("./img/wuk.jpg", game.GetImagePath());
-    }
-
-    // failing test to check if travis works :D
-    @Test
-    public void FirstFailingTest() {
-        Game game = new Game("hey", "some description", "wuk.jpg");
-
-        assertEquals("hey", game.getName());
-        assertEquals("some description", game.getDescription());
-        assertEquals("nono i dont know sorry", game.GetImagePath());
     }
 }
