84655d9bbf44e7dd66df86b20d2d11be33ed78bf
lucas <lucas.coupez@gmail.com>
2016-11-19 18:26:29 +0100

    Create exception for when user is not found

--- src/main/java/GameNationBackEnd/Exceptions/UserNotFoundException.java
+++ src/main/java/GameNationBackEnd/Exceptions/UserNotFoundException.java
@@ -3,5 +3,8 @@ package GameNationBackEnd.Exceptions;
 /**
  * Created by lucas on 19/11/2016.
  */
-public class UserNotFoundException {
+class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String userId) {
+        super("could not find user '" + userId + "'.");
+    }
 }
