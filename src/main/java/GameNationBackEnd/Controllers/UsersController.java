7ea1271215997c15cb0d144407f00e73bdf63fb3
tijs sengier <tijs.sengier@student.odisee.be>
2016-11-23 12:00:53 +0100

    added delete on /users/uid/Games/gid backend everything back on ID!

--- src/main/java/GameNationBackEnd/Controllers/UsersController.java
+++ src/main/java/GameNationBackEnd/Controllers/UsersController.java
@@ -39,16 +39,14 @@ public class UsersController{
     }
 
     // Get all info from database for one specified user (returned as user object)
-    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
-    public User GetUser(@PathVariable String username) {
-
-        return userDB.findByUsername(username);
+    @RequestMapping(value = "/{user}", method = RequestMethod.GET)
+    public User GetUser(@PathVariable User user) {
+        return user;
     }
 
     // Update all info for one specified user (returned as user object)
-    @RequestMapping(value = "/{usern}", method = RequestMethod.POST)
-    public User UpdateUser(@PathVariable String usern, @RequestParam(required = false) String username, @RequestParam(required = false) String email, @RequestParam(required = false) String password, @RequestParam(required = false) String firstname, @RequestParam(required = false) String lastname, @RequestParam(required = false) String teamspeak, @RequestParam(required = false) String discord, @RequestParam(required = false) String description) {
-        User user = userDB.findByUsername(usern);
+    @RequestMapping(value = "/{user}", method = RequestMethod.POST)
+    public User UpdateUser(@PathVariable User user, @RequestParam(required = false) String username, @RequestParam(required = false) String email, @RequestParam(required = false) String password, @RequestParam(required = false) String firstname, @RequestParam(required = false) String lastname, @RequestParam(required = false) String teamspeak, @RequestParam(required = false) String discord, @RequestParam(required = false) String description) {
 
         // Test if the new values aren't empty to prevent loss of data
         if (!username.isEmpty()) { user.setUsername(username); }
@@ -81,18 +79,16 @@ public class UsersController{
         return userDB.findByUsername(username);
     }
 
-    @RequestMapping(value="/{username}/games", method = RequestMethod.GET)
-    public List<UserGame> GetGamesFromUser(@PathVariable String username) {
-        return userGameDB.findByUser(userDB.findByUsername(username));
+    @RequestMapping(value="/{user}/games", method = RequestMethod.GET)
+    public List<UserGame> GetGamesFromUser(@PathVariable User user) {
+        return userGameDB.findByUser(user);
     }
 
     //two functions
     //1. add a list of games to a user with default skill 0
     //2. edit skill of a single game bij giving requestParam skill
-    @RequestMapping(value="/{username}/games", method = RequestMethod.POST)
-    public List<UserGame> AddGameToUser(@PathVariable String username, @RequestParam List<Game> games, @RequestParam(required = false) Integer skill) throws GameAlreadyExistsException {
-
-        User user = userDB.findByUsername(username);
+    @RequestMapping(value="/{user}/games", method = RequestMethod.POST)
+    public List<UserGame> AddGameToUser(@PathVariable User user, @RequestParam List<Game> games, @RequestParam(required = false) Integer skill) throws GameAlreadyExistsException {
 
         //als skill niet ingevuld is moet deze een standaard waarde krijgen.
         if (skill == null) skill = new Integer(0);
@@ -114,10 +110,11 @@ public class UsersController{
         return userGameDB.findByUser(user);
     }
 
-    @RequestMapping(value ="/{user}/games", method= RequestMethod.DELETE)
-    public void InsertGame(@RequestParam Game game, @PathVariable User user) {
+    @RequestMapping(value ="/{user}/games/{game}", method = RequestMethod.DELETE)
+    public String deleteGameFromUser(@PathVariable User user, @PathVariable Game game) {
         UserGame ug = userGameDB.findByUserAndGame(user,game);
         userGameDB.delete(ug);
+        return "deleted";
     }
 
     @RequestMapping(value="/me", method = RequestMethod.GET)
