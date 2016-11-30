bce74a353de68afa6289907b46ee482d643bacd3
lucas <lucas.coupez@gmail.com>
2016-11-30 18:29:08 +0100

    Turn Friend into List<Friend> where necessary

--- src/main/java/GameNationBackEnd/Repositories/FriendRepository.java
+++ src/main/java/GameNationBackEnd/Repositories/FriendRepository.java
@@ -10,8 +10,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * Created by tijs on 30/11/2016.
  */
 public interface FriendRepository extends MongoRepository<Friend, String> {
-    Friend findBySender(User sender);
-    Friend findByReceiver(User receiver);
+    List<Friend> findBySender(User sender);
+    List<Friend> findByReceiver(User receiver);
     Friend findBySenderAndReceiver(User sender, User Receiver);
     List<Friend> findAll();
 }
