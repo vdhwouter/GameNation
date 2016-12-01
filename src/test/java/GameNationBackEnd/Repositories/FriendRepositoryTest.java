fcf93da6a883532a0f7db772ffdbaf5f7177088a
tijs sengier <tijs.sengier@student.odisee.be>
2016-12-01 10:38:47 +0100

    Tests for new repo commands

--- src/test/java/GameNationBackEnd/Repositories/FriendRepositoryTest.java
+++ src/test/java/GameNationBackEnd/Repositories/FriendRepositoryTest.java
@@ -33,10 +33,10 @@ public class FriendRepositoryTest extends TestCase {
     private User user5 = new User("tijs", "tijs@gmail.com", "tijsje123");
 
     private Friend friend1 = new Friend(user1,user2);
-    private Friend friend5 = new Friend(user1,user3);
     private Friend friend2 = new Friend(user2,user3);
     private Friend friend3 = new Friend(user3,user4);
     private Friend friend4 = new Friend(user4,user5);
+    private Friend friend5 = new Friend(user1,user3);
 
     @Before
     public void setUp() {
@@ -81,5 +81,105 @@ public class FriendRepositoryTest extends TestCase {
         Friend friend = friendRepo.findBySenderAndReceiver(user3,user4);
         assertEquals("wouter", friend.getSender().getUsername());
         assertEquals("kjell", friend.getReceiver().getUsername());
+    }
+
+    @Test
+    public void GetAcceptedFriendsSender() {
+        Friend friend6 = new Friend(user3,user1);
+        Friend friend7 = new Friend(user3,user2);
+        Friend friend8 = new Friend(user3,user3);
+        Friend friend9 = new Friend(user3,user4);
+
+        friend6.setAccepted(true);
+        friend7.setAccepted(true);
+        friend8.setAccepted(true);
+        friend9.setAccepted(false);
+
+        friendRepo.deleteAll();
+        friendRepo.save(friend6);
+        friendRepo.save(friend7);
+        friendRepo.save(friend8);
+        friendRepo.save(friend9);
+
+        List<Friend> friends = friendRepo.findBySenderAndAccepted(user3,true);
+        assertEquals("lucas", friends.get(0).getReceiver().getUsername());
+        assertEquals("matthias", friends.get(1).getReceiver().getUsername());
+        assertEquals("wouter", friends.get(2).getReceiver().getUsername());
+    }
+
+    @Test
+    public void GetAcceptedFriendsReceiver() {
+
+        Friend friend6 = new Friend(user1,user3);
+        Friend friend7 = new Friend(user2,user3);
+        Friend friend8 = new Friend(user3,user3);
+        Friend friend9 = new Friend(user4,user3);
+
+        friend6.setAccepted(true);
+        friend7.setAccepted(true);
+        friend8.setAccepted(true);
+        friend9.setAccepted(false);
+
+        friendRepo.deleteAll();
+        friendRepo.save(friend6);
+        friendRepo.save(friend7);
+        friendRepo.save(friend8);
+        friendRepo.save(friend9);
+
+        List<Friend> friends = friendRepo.findByReceiverAndAccepted(user3,true);
+
+        assertEquals("lucas", friends.get(0).getSender().getUsername());
+        assertEquals("matthias", friends.get(1).getSender().getUsername());
+        assertEquals("wouter", friends.get(2).getSender().getUsername());
+    }
+
+    @Test
+    public void GetNotAcceptedFriendsSender() {
+        Friend friend6 = new Friend(user2,user1);
+        Friend friend7 = new Friend(user2,user2);
+        Friend friend8 = new Friend(user2,user3);
+        Friend friend9 = new Friend(user2,user4);
+
+        friend6.setAccepted(false);
+        friend7.setAccepted(false);
+        friend8.setAccepted(false);
+        friend9.setAccepted(true);
+
+        friendRepo.deleteAll();
+        friendRepo.save(friend6);
+        friendRepo.save(friend7);
+        friendRepo.save(friend8);
+        friendRepo.save(friend9);
+
+        List<Friend> friends = friendRepo.findBySenderAndAccepted(user2,false);
+        assertEquals("lucas", friends.get(0).getReceiver().getUsername());
+        assertEquals("matthias", friends.get(1).getReceiver().getUsername());
+        assertEquals("wouter", friends.get(2).getReceiver().getUsername());
+    }
+
+    @Test
+    public void GetNotAcceptedFriendsReceiver() {
+
+        Friend friend6 = new Friend(user1,user2);
+        Friend friend7 = new Friend(user2,user2);
+        Friend friend8 = new Friend(user3,user2);
+        Friend friend9 = new Friend(user4,user2);
+
+        friend6.setAccepted(false);
+        friend7.setAccepted(false);
+        friend8.setAccepted(false);
+        friend9.setAccepted(true);
+
+        friendRepo.deleteAll();
+        friendRepo.save(friend6);
+        friendRepo.save(friend7);
+        friendRepo.save(friend8);
+        friendRepo.save(friend9);
+
+        List<Friend> friends = friendRepo.findByReceiverAndAccepted(user2,false);
+
+        assertEquals("lucas", friends.get(0).getSender().getUsername());
+        assertEquals("matthias", friends.get(1).getSender().getUsername());
+        assertEquals("wouter", friends.get(2).getSender().getUsername());
     }
 }
