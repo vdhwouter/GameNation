42fee3a6a8a4c9159860031a6a87623ff49d2161
lucas <lucas.coupez@gmail.com>
2016-12-05 16:29:26 +0100

    Add friendship button to profile pages

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,26 +1,26 @@
 /* ===========================================
-   show all games that user added
-   =========================================== */
+ show all games that user added
+ =========================================== */
 
-$('#shareProfile').on('click', function(e){
+$('#shareProfile').on('click', function (e) {
     e.preventDefault();
     console.info('Facebook share initiated...');
     logInWithFacebook('http://www.localhost:8080/' + window.profileUser.username);
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     // HACK , CHANGE THI
     var currentUser = window.profileUser;
 
     axios.get("/users/" + currentUser.id + "/games")
-        .then(function(response) {
+        .then(function (response) {
             var data = response.data;
 
             // create html tage with added games
             var ul = document.getElementsByClassName('games-list')[0];
 
-            for(key in data) {
-                if(data.hasOwnProperty(key)) {
+            for (key in data) {
+                if (data.hasOwnProperty(key)) {
                     var value = data[key];
 
                     var li = document.createElement("li");
@@ -51,4 +51,71 @@ $(document).ready(function() {
                 }
             }
         })
+
+    /* FRIEND BUTTON */
+
+    var friendButton = $('#friend-button');
+
+    function updateFriendButton() {
+        if (session.authenticated) {
+            // check if not the profile page of logged in user
+            if (currentUser.id != session.id) {
+                // if there is no relation, show "add as friend" button
+                if (!currentUser.relation) {
+                    friendButton.text("Add as friend");
+                    friendButton.attr('data-user-id', currentUser.id);
+                    friendButton.attr('data-type', 'add');
+                    friendButton.show();
+                    // if there is a relation, not yet accepted...
+                } else if (!currentUser.relation.accepted) {
+                    // ..and authenticated user is sending
+                    if (currentUser.relation.sender.id == session.id) {
+                        friendButton.text("Cancel friend request");
+                        friendButton.attr('data-type', 'delete');
+                        friendButton.show();
+                        // .. and authenticated user is receiving
+                    } else if (currentUser.relation.receiver.id == session.id) {
+                        friendButton.text("Accept friend request");
+                        friendButton.attr('data-type', 'add');
+                        friendButton.show();
+                    }
+                } else {
+                    // users are friends
+                    friendButton.text("End friendship");
+                    friendButton.attr('data-type', 'delete');
+                    friendButton.show();
+                }
+
+            } else {
+                friendButton.hide()
+            }
+        }
+    }
+
+    updateFriendButton();
+
+    friendButton.click(function () {
+        var id = $(this).attr('data-user-id');
+        var type = $(this).attr('data-type');
+
+        switch (type) {
+            case 'add':
+                axios.post('/users/' + session.id + '/friends', {user: currentUser.id})
+                    .then(function (res) {
+                        // reload friendship button with new data
+                        currentUser.relation = res.data;
+                        updateFriendButton();
+                    })
+                break;
+            case 'delete':
+                axios.delete('/users/' + session.id + '/friends/' + currentUser.id)
+                    .then(function (res) {
+                        // reload friendship button with new data
+                        currentUser.relation = res.data;
+                        updateFriendButton();
+                    })
+                break;
+        }
+
+    })
 });
