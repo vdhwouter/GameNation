efa09dc5c5946902fa6dc2d6327e24d059f8c356
Matthias Maes <matthias.maes@outlook.com>
2016-12-11 15:44:19 +0100

    Used common function in functions.js

--- src/main/resources/static/js/functions/functions.js
+++ src/main/resources/static/js/functions/functions.js
@@ -1,38 +1,40 @@
 
-
+// Regular Expressions
 var validEmail = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
 
+// Method called from register route
 var CheckFormInputRegister = function(email, password, confirmation, username){
-    var errors = []
-
-    if (!email.match(validEmail)) errors.push('Email should be a valid email')
-    if (password.length < 6) errors.push('Password should have a minimum length of 6')
-    if (!password.match(/[a-z]/)) errors.push('Password should contain one lowercase letter')
-    if (!password.match(/[A-Z]/)) errors.push('Password should contain one uppercase letter')
-    if (!password.match(/\d/)) errors.push('Password should contain one digit')
-    if (password !== confirmation) errors.push('Password and confirmation should match')
-
-    axios.get('/users?username=' + username).then(res => { if (res.data.length > 0) $('.error-list').append('<li class="error-list__item"><i class="error-list__item__icon fa fa-times-circle" aria-hidden="true"></i><p class="error-list__item__text"> A user with username \'' + username +'\' already exists</p></li>')});
-
-    return errors;
+    var errors = [];
+    if (password !== confirmation) errors.push('Password and confirmation should match');
+    return CommonChecks(email, password, username, errors);
 }
 
 
+// Method called from settings route
 var CheckFormInputSettings = function(email, password, username){
-    var errors = []
+    var errors = [];
+	return CommonChecks(email, password, username, errors);
+}
 
-	if (!email.match(validEmail)) errors.push('Email should be a valid email');
+
+// Commen checks betwee two formchecks
+var CommonChecks = function(email, password, username, errors){
+
+    // Checks on password
 	if (password != null){
-	    if (password.length < 6) errors.push('Password should have a minimum length of 6')
-        if (!password.match(/[a-z]/)) errors.push('Password should contain one lowercase letter')
-        if (!password.match(/[A-Z]/)) errors.push('Password should contain one uppercase letter')
-        if (!password.match(/\d/)) errors.push('Password should contain one digit')
+	    if (password.length < 6) errors.push('Password should have a minimum length of 6');
+        if (!password.match(/[a-z]/)) errors.push('Password should contain one lowercase letter');
+        if (!password.match(/[A-Z]/)) errors.push('Password should contain one uppercase letter');
+        if (!password.match(/\d/)) errors.push('Password should contain one digit');
 	}
 
+	// Check on email
+    if (!email.match(validEmail)) errors.push('Email should be a valid email');
 
-	axios.get('/users?username=' + username).then(res => { if (res.data.length > 0) $('.error-list').append('<li class="error-list__item"><i class="error-list__item__icon fa fa-times-circle" aria-hidden="true"></i><p class="error-list__item__text"> A user with username \'' + username +'\' already exists</p></li>')});
+    // Check on password
+    axios.get('/users?username=' + username).then(res => { if (res.data.length > 0) $('.error-list').append('<li class="error-list__item"><i class="error-list__item__icon fa fa-times-circle" aria-hidden="true"></i><p class="error-list__item__text"> A user with username \'' + username +'\' already exists</p></li>')});
 
-	return errors;
+    // Return all errors
+    return errors;
 }
-
