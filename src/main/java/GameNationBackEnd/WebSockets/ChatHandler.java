c5cc2126af89f0252215af95f6196a808e1cc6eb
Kjell De Backer <kjell.debacker@student.odisee.be>
2016-12-20 19:49:17 +0100

    Fixed final problems with chat

--- src/main/java/GameNationBackEnd/WebSockets/ChatHandler.java
+++ src/main/java/GameNationBackEnd/WebSockets/ChatHandler.java
@@ -7,6 +7,7 @@ import GameNationBackEnd.Repositories.UserRepository;
 import GameNationBackEnd.Repositories.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -34,22 +35,6 @@ public class ChatHandler extends TextWebSocketHandler{
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         User user = userDB.findByUsernameIgnoreCase(session.getPrincipal().getName());
         sessions.put(user.getId(), session);
-
-//        List<ChatPayload> message_list = new ArrayList<>();
-//        msgDB.findByReceiver(user).stream().filter(m -> !m.isRead()).forEach(m -> {
-//             message_list.add(new ChatPayload(m.getSender().getId(),
-//                m.getReceiver().getId(),
-//                m.getMessage()));
-//
-//             m.read();
-//        });
-//        String messages = gson.toJson(message_list, ChatPayload.class);
-//
-//        Thread.sleep(1000);
-//
-//        WebSocketSession s = sessions.get(user.getId());
-//        if (s != null)
-//            s.sendMessage(new TextMessage(messages));
     }
 
     @Override
@@ -86,30 +71,6 @@ public class ChatHandler extends TextWebSocketHandler{
 
                 msgDB.findBySenderAndReceiver(userDB.findOne(chatPayload.getSender()), userDB.findOne(chatPayload.getReceiver()))
                         .stream().filter(m -> !m.isRead()).forEach(m -> m.read());
-            } break;
-
-            case "history": {
-                ChatPayload chatPayload = gson.fromJson(payload.getD(), ChatPayload.class);
-
-                List<Message> message_list = new ArrayList<>();
-
-                PageRequest pagerequest = new PageRequest(0, 30);
-                message_list.addAll(msgDB.findBySenderAndReceiver(userDB.findOne(chatPayload.getSender()), userDB.findOne(chatPayload.getReceiver()), pagerequest));
-                message_list.addAll(msgDB.findBySenderAndReceiver(userDB.findOne(chatPayload.getReceiver()), userDB.findOne(chatPayload.getSender()), pagerequest));
-
-                List<ChatPayload> chat_list = new ArrayList<>();
-
-                message_list.stream().sorted(Comparator.comparing(Message::getTimestamp)).limit(30).forEach(m -> {
-                    chat_list.add(new ChatPayload(m.getSender().getId(),
-                            m.getReceiver().getId(),
-                            m.getMessage()));
-                });
-
-                String messages = gson.toJson(chat_list);
-
-                WebSocketSession s = sessions.get(chatPayload.getSender());
-                if (s != null)
-                    s.sendMessage(new TextMessage(messages));
             } break;
         }
     }
