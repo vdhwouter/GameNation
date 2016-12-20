2379e54f4855cbd24f1b79f1c0a404fc287375a8
lucas <lucas.coupez@gmail.com>
2016-12-20 19:49:45 +0100

    Awkward...

--- src/main/java/GameNationBackEnd/Documents/User.java
+++ src/main/java/GameNationBackEnd/Documents/User.java
@@ -1,5 +1,6 @@
 package GameNationBackEnd.Documents;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
@@ -58,6 +59,7 @@ public class User{
 	public String getEmail() {
 		return email;
 	}
+	@JsonIgnore
 	public String getPassword() {
 		return password;
 	}
